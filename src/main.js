7c453d9fc69e7a0ee145da2a13fc62f9c181ec6c
Jimagu89 <90778682+Jimagu89@users.noreply.github.com>
2024-01-06 13:38:13 -0600

    added iframe

--- src/main.js
+++ src/main.js
@@ -1,18 +1,3 @@
-// // Get the current date
-// const currentDate = new Date();
-
-// // Calculate 61 days from the current date
-// const futureDate = new Date(currentDate);
-// futureDate.setDate(currentDate.getDate() + 61);
-
-// // Format the result (optional)
-// const formattedFutureDate = `${futureDate.getFullYear()}-${(futureDate.getMonth() + 1).toString().padStart(2, '0')}-${futureDate.getDate().toString().padStart(2, '0')}`;
-
-// console.log("Present Date:", currentDate.toISOString().split('T')[0]);
-// console.log("Future Date (61 days later):", formattedFutureDate);
-
-
-// document.getElementById('futureDate').textContent = `${formattedFutureDate}`;
 
 // Function to get the last two Wednesdays from a future date
 function getLastTwoWednesdaysFromFuture(futureDate) {
@@ -49,3 +34,7 @@ const formattedSecondLastWednesday = formatDateString(secondLastWednesday);
 
 // Update the HTML element with the calculated last Wednesdays from the future date
 document.getElementById('lastWednesdaysFromFutureDisplay').textContent = `${formattedLastWednesday}, ${formattedSecondLastWednesday}`;
+
+
+
+
